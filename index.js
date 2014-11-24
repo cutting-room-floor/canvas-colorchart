9b82eeb2870255fcdd11c8e3606833cb5e1c225d
Tom MacWright <tom@macwright.org>
2014-11-24 13:41:39 -0500

    Visual cleanup

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-module.exports = canvasLineChart;
+module.exports = canvasColorChart;
 // var elem = document.createElement('canvas');
 //setInterval(function() {
-//k canvasLineChart(elem, [[10 * (Math.sin(Date.now() / 1000) + 1), 'rgba(0, 0, 0, 0.2)'], [20, '#0f0']], [20, '#FF0000']);
+ // canvasLineChart(elem, [[10, 'rgba(0, 0, 0, 0.2)'], [20, '#0f0']], [10, '#FF0000']);
 //}, 100);
 //=elem
 
-function canvasLineChart(c, data, marker) {
+function canvasColorChart(c, data, marker) {
     var width = 169 * 2;
     var height = 30 * 2;
     var chartHeight = 18 * 2;
+    var margin = 5;
     c.width = width;
     c.height = height;
     c.style.width = width / 2 + 'px';
@@ -18,16 +19,18 @@ function canvasLineChart(c, data, marker) {
     function xScale(_) {
         return (_ / 20);
     }
-
+    function xScalePX(_) {
+        return ~~Math.min(Math.max(0, ((_ / 20) * (width - (margin * 2))) + margin), width - margin);
+    }
     ctx.fillStyle = '#fff';
-    [width - chartHeight / 2, chartHeight / 2].forEach(function(offset) {
+    [width - (chartHeight / 2) - margin, margin + chartHeight / 2].forEach(function(offset) {
         ctx.beginPath();
         ctx.arc(offset, chartHeight/2 - 1, chartHeight/2 - 1, 0, 2 * Math.PI, false);
         ctx.fill();
         ctx.stroke();
     });
 
-    ctx.fillRect(chartHeight/2, 1, width - chartHeight, chartHeight - 2);
+    ctx.fillRect(chartHeight/2 + margin, 1, width - chartHeight - margin, chartHeight - 2);
 
     ctx.fillStyle = '#000';
     ctx.fillRect(chartHeight/2, 0, width - chartHeight, 1);
@@ -37,10 +40,10 @@ function canvasLineChart(c, data, marker) {
     ctx.globalCompositeOperation = 'source-atop';
     ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
     for (var i = 0; i < 20; i++) {
-        ctx.fillRect(xScale(i) * width, 0, 1, height);
+        ctx.fillRect(xScalePX(i), 0, 2, height);
     }
 
-    var grd = ctx.createLinearGradient(0, 0, width, 0);
+    var grd = ctx.createLinearGradient(margin, 0, width - margin, 0);
     data.forEach(function(data, i) {
         grd.addColorStop(xScale(data[0]), data[1]);
     });
@@ -52,14 +55,14 @@ function canvasLineChart(c, data, marker) {
 
     if (marker) {
         ctx.fillStyle = '#3bb2d0';
-        ctx.fillRect(Math.min(width - 3, xScale(marker[0]) * width), 0, 3, height);
-        var xAnchor = xScale(marker[0]) * width;
+        ctx.fillRect(xScalePX(marker[0]), 0, 3, height);
+        var xAnchor = xScalePX(marker[0]);
 
         var labelWidth = 230;
         var labelWidthH = labelWidth / 2;
 
         if (xAnchor < labelWidthH) xAnchor = labelWidthH;
-        if (xAnchor > (width - labelWidthH)) xAnchor = width - labelWidthH;
+        if (xAnchor > (width - labelWidthH)) xAnchor = width - labelWidthH - margin;
         ctx.fillStyle = '#3bb2d0';
         ctx.fillRect(xAnchor - labelWidthH, chartHeight, labelWidth, height - chartHeight);
         ctx.fillStyle = '#fff';
